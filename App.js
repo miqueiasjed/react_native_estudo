254541ac34fd1d8b5a1f284fcc54a9019fa33277
Miqueias Mesquita <miqueias_jed@hotmail.com>
2023-12-04 18:29:57 -0300

    aplicativo quebra biscoito

--- App.js
+++ App.js
@@ -1,61 +1,154 @@
 import React, { Component, useState } from "react";
-import { View, Text, TextInput, StyleSheet, Button } from "react-native";
+import { View, Text, TextInput, StyleSheet, Button, Image, TouchableOpacity } from "react-native";
 
-// Trabalhando com botao e input
+// APP BISCOITO DA SORTE
 function App(){
 
-  const [nome, setNome] = useState('');
-  const [input, setInput] = useState('');
+  const [img, setImg] = useState(require('./src/biscoito.png'));
+  const [textoFrase, setTextoFrase] = useState('');
 
-  function entrar(){
+  let frases = [
+    "A persistência leva à conquista.",
+    "O sucesso sorri para aqueles que trabalham duro.",
+    "Grandes desafios trazem grandes recompensas.",
+    "Acredite nos seus sonhos e siga em frente.",
+    "A sorte favorece a mente preparada.",
+    "A paciência é a chave para superar obstáculos.",
+    "O melhor está por vir; continue persistindo.",
+  ];
 
-    if(input === ''){
-      Alert('Digite seu nome!');
-      return;
-    }
+  function quebraBiscoito()
+  {
+    let numeroAleatorio = Math.floor(Math.random() * frases.length);
 
-    setNome('Bem vindo: ' + input);
+    setTextoFrase('" ' + frases[numeroAleatorio] + ' "');
+    setImg(require('./src/biscoitoAberto.png'));
+  }
 
+  function reiniciaBiscoito(){
+    setTextoFrase('');
+    setImg(require('./src/biscoito.png'));
   }
 
   return(
-    <View style = {styles.container}>
+    <View style={styles.container}>
 
-      <TextInput
-        style = {styles.input}
-        placeholder="Digite seu Nome"
-        onChangeText={(text) => setInput(text)}
+      <Image
+        source={img}
+        style={styles.img}
       />
 
-      <Text style = {styles.texto}> {nome} </Text>
+      <Text style={styles.textoFrase}> {textoFrase} </Text>
 
-      <Button title="Entrar" onPress={entrar} />
+      <TouchableOpacity style={styles.botao} onPress={ quebraBiscoito }>
+        <View style = {styles.btnArea}>
+          <Text style = {styles.btnTexto} >Quebrar Biscoito</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={[styles.botao, {marginTop: 15, borderColor: '#121212'}]} onPress={ reiniciaBiscoito }>
+        <View style = {styles.btnArea}>
+          <Text style = {[styles.btnTexto, {color: '#121212'}]} >Reiniciar Biscoito</Text>
+        </View>
+      </TouchableOpacity>
 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
+    flex:1, 
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  img: {
+    width: 230,
+    height: 230
+  },
+  textoFrase: {
+    fontSize: 20, 
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  botao: {
+    width: 230,
+    height: 50,
+    borderColor: '#dd7b22',
+    borderWidth: 2,
+    borderRadius: 25
+  },
+  btnArea: {
     flex: 1,
-    marginTop: 50
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  input:{
-    color: 'red',
-    height: 45,
-    borderWidth: 1,
-    margin: 10,
-    padding: 20,
-    fontSize: 20
-  },
-  texto:{
-    textAlign: 'center',
-    fontSize: 25,
-    marginTop: 15
+  btnTexto: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: '#dd7b22'
   }
 });
 
 export default App;
+
+// Trabalhando com botao e input
+// function App(){
+
+//   const [nome, setNome] = useState('');
+//   const [input, setInput] = useState('');
+
+//   function entrar(){
+
+//     if(input === ''){
+//       Alert('Digite seu nome!');
+//       return;
+//     }
+
+//     setNome('Bem vindo: ' + input);
+
+//   }
+
+//   return(
+//     <View style = {styles.container}>
+
+//       <TextInput
+//         style = {styles.input}
+//         placeholder="Digite seu Nome"
+//         onChangeText={(text) => setInput(text)}
+//       />
+
+//       <Text style = {styles.texto}> {nome} </Text>
+
+//       <Button title="Entrar" onPress={entrar} />
+
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container:{
+//     flex: 1,
+//     marginTop: 50
+//   },
+//   input:{
+//     color: 'red',
+//     height: 45,
+//     borderWidth: 1,
+//     margin: 10,
+//     padding: 20,
+//     fontSize: 20
+//   },
+//   texto:{
+//     textAlign: 'center',
+//     fontSize: 25,
+//     marginTop: 15
+//   }
+// });
+
+// export default App;
 
 //Recebendo dados Text Input
 // function App(){
